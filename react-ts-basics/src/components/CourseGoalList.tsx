068462c520fcd7667d829e52a10289c71752ffd1
Pavan S <iampavan05@gmail.com>
2024-08-03 20:27:43 +0530

    added info-box UI & logic

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,8 @@
+import { type ReactNode } from "react";
+
 import CourseGoal from "./CourseGoal.tsx";
 import { CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InfoBox.tsx";
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -9,19 +12,40 @@ type CourseGoalListProps = {
 
 export default function CourseGoalList(props: CourseGoalListProps) {
   const { goals, onDeleteGoal } = props;
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {/* TypeScript understands that the goal which we get
-      as a parameter in the map function, will be of
-      type CourseGoal
-  */}
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {/* TypeScript understands that the goal which we get
+          as a parameter in the map function, will be of
+          type CourseGoal
+      */}
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
---
+++ react-ts-basics/src/components/InfoBox.tsx
@@ -0,0 +1,29 @@
+import { type ReactNode } from "react";
+
+type InfoBoxProps = {
+  // Mode should be either hint or warning,
+  // So we could use union type and the literal type feature.
+  // So we don't just say, that this should be a type string,
+  // But instead, we have 2 specific strings that we want to accept.
+  mode: "hint" | "warning";
+  children: ReactNode;
+};
+
+export default function InfoBox(props: InfoBoxProps) {
+  const { mode, children } = props;
+
+  if (mode === "hint") {
+    return (
+      <aside className="infobox infobox-hint">
+        <p>{children}</p>
+      </aside>
+    );
+  }
+
+  return (
+    <aside className="infobox infobox-warning warning--medium">
+      <h2>Warning</h2>
+      <p>{children}</p>
+    </aside>
+  );
+}
