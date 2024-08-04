dacfc300e54bb58004cc4eed5572002fa6417adc
Pavan S <iampavan05@gmail.com>
2024-08-04 12:36:35 +0530

    discriminated unions

--- react-ts-basics/src/components/InfoBox.tsx
+++ react-ts-basics/src/components/InfoBox.tsx
@@ -1,17 +1,20 @@
 import { type ReactNode } from "react";
 
-type InfoBoxProps = {
-  // Mode should be either hint or warning,
-  // So we could use union type and the literal type feature.
-  // So we don't just say, that this should be a type string,
-  // But instead, we have 2 specific strings that we want to accept.
-  mode: "hint" | "warning";
-  severity?: "low" | "medium" | "high"; // ? meaning, the prop is optional.
+type HintBoxProps = {
+  mode: "hint";
   children: ReactNode;
 };
 
+type WarningBoxProps = {
+  mode: "warning";
+  severity: "low" | "medium" | "high";
+  children: ReactNode;
+};
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
 export default function InfoBox(props: InfoBoxProps) {
-  const { mode, children, severity } = props;
+  const { mode, children } = props;
 
   if (mode === "hint") {
     return (
@@ -21,6 +24,7 @@ export default function InfoBox(props: InfoBoxProps) {
     );
   }
 
+  const { severity } = props;
   return (
     <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
