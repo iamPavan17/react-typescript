61782256e2f91cc41be2fd9e38cbae9d92d380cc
Pavan S <iampavan05@gmail.com>
2024-08-06 20:15:54 +0530

    reusable input comp

--- react-advanced-components/src/App.tsx
+++ react-advanced-components/src/App.tsx
@@ -1,5 +1,12 @@
+import Input from "./components/Input.tsx";
+
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <>
+      <Input id="name" label="Your name" type="text" />
+      <Input id="age" label="Your age" type="number" />
+    </>
+  );
 }
 
 export default App;
--- react-advanced-components/src/components/Input.tsx
+++ react-advanced-components/src/components/Input.tsx
@@ -1,3 +1,17 @@
-export default function Input() {
-  return <div>Input</div>;
+import { type ComponentPropsWithoutRef } from "react";
+
+type InputProps = {
+  label: string;
+  id: string;
+} & ComponentPropsWithoutRef<"input">;
+
+export default function Input(props: InputProps) {
+  const { id, label, ...rest } = props;
+
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input type="text" id={id} {...rest} />
+    </p>
+  );
 }
