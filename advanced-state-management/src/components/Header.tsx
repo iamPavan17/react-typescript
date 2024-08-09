03304ff0db585dec657265e823cd1ae17c27192a
Pavan S <iampavan05@gmail.com>
2024-08-09 23:34:58 +0530

    useContext & useReducer

--- advanced-state-management/src/components/Header.tsx
+++ advanced-state-management/src/components/Header.tsx
@@ -1,11 +1,22 @@
-import Button from './UI/Button.tsx';
+import Button from "./UI/Button.tsx";
+import { useTimersContext } from "../store/timer-context.tsx";
 
 export default function Header() {
+  const timersContext = useTimersContext();
+
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button
+        onClick={
+          timersContext.isRunning
+            ? timersContext.stopTimers
+            : timersContext.startTimers
+        }
+      >
+        {timersContext.isRunning ? "Stop" : "Start"} Timers
+      </Button>
     </header>
   );
 }
